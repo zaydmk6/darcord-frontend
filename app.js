c2b726998e36b861cb5a012a35d5a6e6187c0a35
zaydmk6 <zaydmk950@gmail.com>
2026-02-13 20:16:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 const API = "https://darcord-backend.up.railway.app"
-
 let token = null
 let socket = null
-let currentChannel = 1
+let server_id = 1
+let channel_id = 1
 
 async function register(){
   await fetch(API+"/register",{
     method:"POST",
     headers:{"Content-Type":"application/json"},
-    body:JSON.stringify({
-      username:username.value,
-      password:password.value
-    })
+    body:JSON.stringify({username:username.value,password:password.value})
   })
   alert("Registered")
 }
@@ -20,10 +17,7 @@ async function login(){
   const res = await fetch(API+"/login",{
     method:"POST",
     headers:{"Content-Type":"application/json"},
-    body:JSON.stringify({
-      username:username.value,
-      password:password.value
-    })
+    body:JSON.stringify({username:username.value,password:password.value})
   })
   const data = await res.json()
   token = data.token
@@ -31,17 +25,29 @@ async function login(){
 }
 
 function connect(){
-  socket = new WebSocket(API.replace("https","wss")+"/ws/"+currentChannel)
+  socket = new WebSocket(API.replace("https","wss")+"/ws/"+channel_id)
+
   socket.onmessage = (e)=>{
     const msg = JSON.parse(e.data)
-    messages.innerHTML += `<p><b>${msg.username}:</b> ${msg.message}</p>`
+
+    if(msg.type === "chat"){
+      messages.innerHTML += `<p><b>${msg.username}</b>: ${msg.message}</p>`
+    }
+
+    if(msg.type === "online"){
+      onlineUsers.innerHTML = ""
+      msg.users.forEach(u=>{
+        onlineUsers.innerHTML += `<div class="user">${u}</div>`
+      })
+    }
   }
 }
 
 function sendMessage(){
   socket.send(JSON.stringify({
     token:token,
-    message:message.value
+    message:message.value,
+    server_id:server_id
   }))
 }
 
@@ -50,10 +56,7 @@ async function createServer(){
   await fetch(API+"/create_server",{
     method:"POST",
     headers:{"Content-Type":"application/json"},
-    body:JSON.stringify({
-      token:token,
-      name:name
-    })
+    body:JSON.stringify({token:token,name:name})
   })
-  alert("Server created")
+  alert("Created")
 }
