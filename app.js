94f2a35ef5fb2159763f08c486b745fe9796ad13
zaydmk6 <zaydmk950@gmail.com>
2026-02-13 20:09:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,59 @@
-let token = null
-let socket = null
 const API = "https://darcord-backend.up.railway.app"
 
-async function register() {
-  await fetch(API + "/register", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({
-      username: username.value,
-      password: password.value
+let token = null
+let socket = null
+let currentChannel = 1
+
+async function register(){
+  await fetch(API+"/register",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({
+      username:username.value,
+      password:password.value
     })
   })
+  alert("Registered")
 }
 
-async function login() {
-  const res = await fetch(API + "/login", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({
-      username: username.value,
-      password: password.value
+async function login(){
+  const res = await fetch(API+"/login",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({
+      username:username.value,
+      password:password.value
     })
   })
   const data = await res.json()
   token = data.token
-  connectWS()
+  connect()
 }
 
-function connectWS() {
-  socket = new WebSocket(API.replace("https", "wss") + "/ws")
-
-  socket.onmessage = (event) => {
-    const msg = JSON.parse(event.data)
-    chat.innerHTML += `<p><b>${msg.username}:</b> ${msg.message}</p>`
+function connect(){
+  socket = new WebSocket(API.replace("https","wss")+"/ws/"+currentChannel)
+  socket.onmessage = (e)=>{
+    const msg = JSON.parse(e.data)
+    messages.innerHTML += `<p><b>${msg.username}:</b> ${msg.message}</p>`
   }
 }
 
-function sendMessage() {
+function sendMessage(){
   socket.send(JSON.stringify({
-    token: token,
-    message: message.value
+    token:token,
+    message:message.value
   }))
+}
+
+async function createServer(){
+  const name = prompt("Server name?")
+  await fetch(API+"/create_server",{
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body:JSON.stringify({
+      token:token,
+      name:name
+    })
+  })
+  alert("Server created")
 }
