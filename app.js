36507adf7c78bcd7d06c39500f967baba29a4133
zaydmk6 <zaydmk950@gmail.com>
2026-02-13 20:26:57 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,62 +1,46 @@
-const API = "https://darcord-backend.up.railway.app"
-let token = null
-let socket = null
-let server_id = 1
-let channel_id = 1
+const API="https://darcord-backend.up.railway.app"
+
+let token=null
+let socket=null
+let channel_id=1
 
 async function register(){
-  await fetch(API+"/register",{
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body:JSON.stringify({username:username.value,password:password.value})
-  })
-  alert("Registered")
+ await fetch(API+"/register",{method:"POST",
+ headers:{"Content-Type":"application/json"},
+ body:JSON.stringify({username:username.value,password:password.value})})
+ alert("Registered")
 }
 
 async function login(){
-  const res = await fetch(API+"/login",{
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body:JSON.stringify({username:username.value,password:password.value})
-  })
-  const data = await res.json()
-  token = data.token
-  connect()
+ const res=await fetch(API+"/login",{method:"POST",
+ headers:{"Content-Type":"application/json"},
+ body:JSON.stringify({username:username.value,password:password.value})})
+ const data=await res.json()
+ token=data.token
+ connect()
 }
 
 function connect(){
-  socket = new WebSocket(API.replace("https","wss")+"/ws/"+channel_id)
+ socket=new WebSocket(API.replace("https","wss")+"/ws/"+channel_id)
 
-  socket.onmessage = (e)=>{
-    const msg = JSON.parse(e.data)
-
-    if(msg.type === "chat"){
-      messages.innerHTML += `<p><b>${msg.username}</b>: ${msg.message}</p>`
-    }
-
-    if(msg.type === "online"){
-      onlineUsers.innerHTML = ""
-      msg.users.forEach(u=>{
-        onlineUsers.innerHTML += `<div class="user">${u}</div>`
-      })
-    }
-  }
+ socket.onmessage=(e)=>{
+  const msg=JSON.parse(e.data)
+  messages.innerHTML+=`<p><b>${msg.username}</b>: ${msg.message}</p>`
+ }
 }
 
 function sendMessage(){
-  socket.send(JSON.stringify({
-    token:token,
-    message:message.value,
-    server_id:server_id
-  }))
+ socket.send(JSON.stringify({
+  token:token,
+  message:message.value
+ }))
+ message.value=""
 }
 
 async function createServer(){
-  const name = prompt("Server name?")
-  await fetch(API+"/create_server",{
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body:JSON.stringify({token:token,name:name})
-  })
-  alert("Created")
+ const name=prompt("Server name?")
+ await fetch(API+"/create_server",{method:"POST",
+ headers:{"Content-Type":"application/json"},
+ body:JSON.stringify({token:token,name:name})})
+ alert("Created")
 }
